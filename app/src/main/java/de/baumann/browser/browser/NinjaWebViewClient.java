ff0c2adcf88333e52a8780d14a7d2ca6de1c6a0f
woheller69 <wolfgang.heller@web.de>
2021-06-21 18:45:11 +0200

    Fix bug for tablet with large screen size

--- app/src/main/java/de/baumann/browser/browser/NinjaWebViewClient.java
+++ app/src/main/java/de/baumann/browser/browser/NinjaWebViewClient.java
@@ -84,7 +84,7 @@ public class NinjaWebViewClient extends WebViewClient {
 
     @Override
     public void onLoadResource(WebView view, String url) {
-        view.evaluateJavascript("document.querySelector('meta[name=\"viewport\"]').setAttribute('content', 'width=1024px, initial-scale=' + (document.documentElement.clientWidth / 1024));", null);
+       if (view.getWidth()<1300) view.evaluateJavascript("document.querySelector('meta[name=\"viewport\"]').setAttribute('content', 'width=1200px, initial-scale=' + (document.documentElement.clientWidth / 1200));", null);
     }
 
     @Override

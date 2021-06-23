298b61f99e7132f7c619f3ca4033d77e6213a795
woheller69 <wolfgang.heller@web.de>
2021-06-23 12:05:57 +0200

    Option for autoupdate of hosts.txt once per day

--- app/src/main/java/de/baumann/browser/browser/AdBlock.java
+++ app/src/main/java/de/baumann/browser/browser/AdBlock.java
@@ -2,33 +2,105 @@ package de.baumann.browser.browser;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import android.util.Log;
 
+import androidx.preference.PreferenceManager;
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.*;
 
 public class AdBlock {
     private static final String FILE = "hosts.txt";
+    private static final String hostURL = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
     private static final Set<String> hosts = new HashSet<>();
     @SuppressLint("ConstantLocale")
     private static final Locale locale = Locale.getDefault();
 
     private static void loadHosts(final Context context) {
         Thread thread = new Thread(() -> {
-            AssetManager manager = context.getAssets();
             try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(manager.open(FILE)));
+                File file = new File(context.getDir("filesdir", Context.MODE_PRIVATE) + "/"+FILE);
+                FileReader in = new FileReader(file);
+                BufferedReader reader = new BufferedReader(in) ;
                 String line;
                 while ((line = reader.readLine()) != null) {
                     hosts.add(line.toLowerCase(locale));
                 }
+                in.close();
             } catch (IOException i) {
                 Log.w("browser", "Error loading hosts", i);
+            }
+        });
+        thread.start();
+    }
+
+    private static void downloadHosts(final Context context) {
+        Thread thread = new Thread(() -> {
+            try {
+                URL url = new URL(hostURL);
+                Log.d("browser","Download AdBlock hosts");
+                URLConnection ucon = url.openConnection();
+                ucon.setReadTimeout(5000);
+                ucon.setConnectTimeout(10000);
+
+                InputStream is = ucon.getInputStream();
+                BufferedInputStream inStream = new BufferedInputStream(is, 1024 * 5);
+
+                File tempfile = new File(context.getDir("filesdir", Context.MODE_PRIVATE) + "/temp.txt");
+
+                if (tempfile.exists())
+                {
+                    tempfile.delete();
+                }
+                tempfile.createNewFile();
+
+                FileOutputStream outStream = new FileOutputStream(tempfile);
+                byte[] buff = new byte[5 * 1024];
+
+                int len;
+                while ((len = inStream.read(buff)) != -1)
+                {
+                    outStream.write(buff, 0, len);
+                }
+
+                outStream.flush();
+                outStream.close();
+                inStream.close();
+
+                //now remove leading 0.0.0.0 from file
+                FileReader in = new FileReader(tempfile);
+                BufferedReader reader = new BufferedReader(in) ;
+                File outfile = new File(context.getDir("filesdir", Context.MODE_PRIVATE) + "/"+FILE);
+                FileWriter out = new FileWriter(outfile);
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    if (line.startsWith("0.0.0.0 ")) {
+                        line=line.substring(8);
+                    }
+                    out.write(line+"\n");
+                }
+                in.close();
+                out.close();
+
+                tempfile.delete();
+
+                loadHosts(context);  //reload hosts after update
+
+            } catch (IOException i) {
+                Log.w("browser", "Error updating AdBlock hosts", i);
             }
         });
         thread.start();
@@ -51,10 +123,42 @@ public class AdBlock {
     }
 
     public AdBlock(Context context) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+        File file = new File(context.getDir("filesdir", Context.MODE_PRIVATE) + "/"+FILE);
+        if (!file.exists()) { //copy hosts.txt from assets if not available
+            Log.d("Hosts file","does not exist");
+            try {
+                AssetManager manager = context.getAssets();
+                copyFile(manager.open(FILE), new FileOutputStream(file));
+                downloadHosts(context);  //try to update hosts.txt from internet
+            }        catch(IOException e) {
+                Log.e("browser", "Failed to copy asset file", e);
+            }
+        }
+
+        if (sp.getBoolean("sp_ad_block_update", false))
+        {
+            Calendar time = Calendar.getInstance();
+            time.add(Calendar.DAY_OF_YEAR,-1);
+            Date lastModified = new Date(file.lastModified());
+            if(lastModified.before(time.getTime())) {
+                //update if file is older than a day
+                downloadHosts(context);
+            }
+        }
         if (hosts.isEmpty()) {
             loadHosts(context);
         }
     }
+
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+    }
+
 
     boolean isAd(String url) {
         String domain;

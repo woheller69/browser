477ee87be019793905b4a0c36d9e34fd861a2042
woheller69 <wolfgang.heller@web.de>
2021-07-04 19:08:19 +0200

    Fix null object reference exception in SwipeTouchListener. Maybe try...catch could also be removed.

--- app/src/main/java/de/baumann/browser/view/SwipeTouchListener.java
+++ app/src/main/java/de/baumann/browser/view/SwipeTouchListener.java
@@ -36,31 +36,32 @@ public class SwipeTouchListener implements OnTouchListener {
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
             boolean result = false;
-            try {
-                float diffY = e2.getY() - e1.getY();
-                float diffX = e2.getX() - e1.getX();
-                if (Math.abs(diffX) > Math.abs(diffY)) {
-                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-                        if (diffX > 0) {
-                            onSwipeRight();
+            if (e1!=null && e2!=null) {
+                try {
+                    float diffY = e2.getY() - e1.getY();
+                    float diffX = e2.getX() - e1.getX();
+                    if (Math.abs(diffX) > Math.abs(diffY)) {
+                        if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                            if (diffX > 0) {
+                                onSwipeRight();
+                            } else {
+                                onSwipeLeft();
+                            }
+                        }
+                        result = true;
+                    } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffY > 0) {
+                            onSwipeBottom();
                         } else {
-                            onSwipeLeft();
+                            onSwipeTop();
                         }
                     }
                     result = true;
-                }
-                else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
-                    if (diffY > 0) {
-                        onSwipeBottom();
-                    } else {
-                        onSwipeTop();
-                    }
-                }
-                result = true;
 
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }else result =false;
             return result;
         }
     }

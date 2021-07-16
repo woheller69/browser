276faf05e0641d647d0268dfc474308cdf546ca0
juergen <baumann.juergen@posteo.de>
2021-07-16 21:16:58 +0200

    8.1 release

--- app/build/generated/source/buildConfig/debug/de/baumann/browser/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/de/baumann/browser/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "de.baumann.browser";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 113;
-  public static final String VERSION_NAME = "8.0";
+  public static final int VERSION_CODE = 114;
+  public static final String VERSION_NAME = "8.1";
 }
